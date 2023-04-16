a70083566a6dcc8b828d7af62f5371647ab9e708
earthwebdev <earthweb21st@gmail.com>
2023-04-16 14:13:56 +0545

    palidrome assignment using array conversion

--- day17/palidrome.js
+++ day17/palidrome.js
@@ -3,7 +3,7 @@ let btnSubmit = document.getElementById('btnSubmit');
 if (btnSubmit != '') {
     btnSubmit.addEventListener('click', (event) =>{
         event.preventDefault();
-        
+        console.log('string conversion method');
         let numstr = document.getElementById('numstr').value;
         console.log('btn clickecd '+ numstr);
         let revname = '';
@@ -19,6 +19,8 @@ if (btnSubmit != '') {
     else{
         document.querySelector("p.palidrome").innerHTML = numstr + ' is not the palidrome number/string';
     }
+
+    arrayConversionMethod(numstr);
         //let studentli = document.createTextNode(studentName);
 //console.log(document.querySelector("ul"));
 //console.log(document.querySelector("p.palidrome"));
@@ -26,4 +28,24 @@ if (btnSubmit != '') {
         //document.querySelector("p.palidrome").innerHTML('yes it is palidrome');
 
     } );    
+}
+
+function arrayConversionMethod(str){
+    let numstr = str;
+    if(str !== null){
+        console.log('convert string to array and check the palidrom function ');
+        let strToArray = str.split("");
+        console.log(strToArray );
+        strToArray.reverse();
+        console.log(strToArray );
+        let reverseStr = strToArray.join("");
+        console.log(reverseStr + "  ==- "+ str );
+        if(reverseStr === str){
+            document.querySelector("p.palidromeArray").innerHTML = numstr + ' is the palidrome number/string';
+        }
+        else{
+            document.querySelector("p.palidromeArray").innerHTML = numstr + ' is not the palidrome number/string';
+        }
+    }
+   
 }
